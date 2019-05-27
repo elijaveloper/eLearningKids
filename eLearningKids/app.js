1fd36258d6abc8a34b82aceabe5bc0457e8ea46b
elijaveloper <elijaveloper@gmail.com>
2019-05-27 15:49:44 +0700

    POST working

--- eLearningKids/app.js
+++ eLearningKids/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/r_users');
+var usersRouter = require('./routes/userRouter');
 
 var app = express();
 
---
+++ eLearningKids/controllers/userController.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const UserModel = require("../models/schemas.js").User;
+
+mongoose.connect("mongodb+srv://dbELK_elijah:secretpassword@elearningkids-zdukp.gcp.mongodb.net/test?retryWrites=true", {useNewUrlParser:true});
+
+class UserController {
+    static async login(req,res){
+        try{
+            let result = await UserModel.findOne({username:req.body.username,password:req.body.password}).exec();
+            res.send(result);
+        }catch(exception){
+            res.status(500).send(exception)
+        }
+    }
+}
+
+var conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+module.exports = UserController;
